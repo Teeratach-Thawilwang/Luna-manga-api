c42ca33d8498df06f9f1d1f877f8901da712342b
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-07-26 15:56:11 +0700

    Add unique constraints to column 'name' and 'type'.

--- main/app/Domain/Category/Models/Category.py
+++ main/app/Domain/Category/Models/Category.py
@@ -5,7 +5,7 @@ from django.forms.models import model_to_dict
 
 class Category(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(unique=True, max_length=150, db_collation="utf8mb4_unicode_ci")
+    name = models.CharField(max_length=150, db_collation="utf8mb4_unicode_ci")
     type = models.CharField(max_length=150)
     status = models.CharField(max_length=30, default="inactive")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,6 +15,7 @@ class Category(models.Model):
 
     class Meta:
         db_table = "categories"
+        constraints = [models.UniqueConstraint(fields=["name", "type"], name="unique_name_type")]
 
     def data(self):
         return model_to_dict(self)
