8e64bdc6707c6ac934d298e79390d777f8ce406a
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-08-15 11:46:58 +0700

    Update command to handle group by cases.

--- app/app/management/commands/UpdateWidgetChapterGroup.py
+++ app/app/management/commands/UpdateWidgetChapterGroup.py
@@ -10,33 +10,46 @@ from app.Enums.WidgetTypeEnum import WidgetTypeEnum
 
 """
 To run command
-shell  run => python manage.py UpdateWidgetChapterGroup
-docker run => docker-compose exec app python manage.py UpdateWidgetChapterGroup
+shell  run => python manage.py UpdateWidgetChapterGroup 2
+docker run => docker-compose exec app python manage.py UpdateWidgetChapterGroup 2
 """
 
 
 class Command(BaseCommand):
     help = f"Update widget type={WidgetTypeEnum.CHAPTER_GROUP}."
 
+    def add_arguments(self, parser):
+        hint = """
+        1. Group by story.
+        2. No Group by.
+        3. Group by story and sort by banner name.
+        """
+        parser.add_argument("Choice", type=int, help=hint, nargs="?", default=1)
+
     def handle(self, *args, **kwargs):
-        # Group by story
-        # subquery = Banner.objects.filter(type=BannerTypeEnum.CHAPTER).values("title").annotate(max_id=Max("id")).values("max_id")
-        # bannerIdsDict = Banner.objects.filter(id__in=subquery, type=BannerTypeEnum.CHAPTER).order_by("-id").values("id")
+        choice = kwargs["Choice"]
 
-        # No Group by
-        # bannerIdsDict = Banner.objects.filter(type=BannerTypeEnum.CHAPTER).order_by("-id").values("id")[:100]
+        if choice == 1:
+            print("Group by story.")
+            subquery = Banner.objects.filter(type=BannerTypeEnum.CHAPTER).values("title").annotate(max_id=Max("id")).values("max_id")
+            bannerIdsDict = Banner.objects.filter(id__in=subquery, type=BannerTypeEnum.CHAPTER).order_by("-id").values("id")
 
-        # Group by story, sort by banner name
-        maxNameNumberSubquery = (
-            Banner.objects.filter(type=BannerTypeEnum.CHAPTER, title=OuterRef("title"))
-            .annotate(name_number=Cast(Substr("name", 7), IntegerField()))
-            .values("title")
-            .annotate(max_name_number=Max("name_number"))
-            .values("max_name_number")
-        )
-        bannersWithMaxNameNumber = Banner.objects.annotate(name_number=Cast(Substr("name", 7), IntegerField())).filter(type=BannerTypeEnum.CHAPTER, name_number=Subquery(maxNameNumberSubquery))
-        groupedBannersSubquery = bannersWithMaxNameNumber.values("title").annotate(max_id=Max("id")).values("max_id")
-        bannerIdsDict = Banner.objects.filter(id__in=Subquery(groupedBannersSubquery), type=BannerTypeEnum.CHAPTER).order_by("-id").values("id")
+        if choice == 2:
+            print("No Group by.")
+            bannerIdsDict = Banner.objects.filter(type=BannerTypeEnum.CHAPTER).order_by("-id").values("id")[:100]
+
+        if choice == 3:
+            print("Group by story and sort by banner name.")
+            maxNameNumberSubquery = (
+                Banner.objects.filter(type=BannerTypeEnum.CHAPTER, title=OuterRef("title"))
+                .annotate(name_number=Cast(Substr("name", 7), IntegerField()))
+                .values("title")
+                .annotate(max_name_number=Max("name_number"))
+                .values("max_name_number")
+            )
+            bannersWithMaxNameNumber = Banner.objects.annotate(name_number=Cast(Substr("name", 7), IntegerField())).filter(type=BannerTypeEnum.CHAPTER, name_number=Subquery(maxNameNumberSubquery))
+            groupedBannersSubquery = bannersWithMaxNameNumber.values("title").annotate(max_id=Max("id")).values("max_id")
+            bannerIdsDict = Banner.objects.filter(id__in=Subquery(groupedBannersSubquery), type=BannerTypeEnum.CHAPTER).order_by("-id").values("id")
 
         bannerIds = [banner["id"] for banner in bannerIdsDict]
 
