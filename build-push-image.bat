eac3f00494db3e6aa1c86f18752af2e18a873563
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-08-18 04:30:55 +0700

    Update built & deploy image script.

--- build-push-image.bat
+++ build-push-image.bat
@@ -1,4 +1,4 @@
 
 call docker-compose up -d --build
-docker tag luna_manga_api teeratachdocker/luna_manga_api:latest
-docker push teeratachdocker/luna_manga_api:latest
+call docker tag luna_manga_api teeratachdocker/luna_manga_api:latest
+call docker push teeratachdocker/luna_manga_api:latest
