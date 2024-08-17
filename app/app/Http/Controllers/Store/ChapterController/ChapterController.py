fe77a232a87d70e69b403e590e692107f18a0fb5
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-08-18 04:47:50 +0700

    Using manual cache on chapter show API. Because of collecting view_count is important.

--- app/app/Http/Controllers/Store/ChapterController/ChapterController.py
+++ app/app/Http/Controllers/Store/ChapterController/ChapterController.py
@@ -12,6 +12,7 @@ from app.Enums.StatusEnum import ChapterStatusEnum
 from app.Exceptions.ResourceNotFoundException import ResourceNotFoundException
 from app.Http.Resources.Store.ChapterController.ChapterResource import ChapterResource
 from app.Middlewares.AuthenticationMiddleware import AuthenticationMiddleware
+from app.Services.Helpers import getCache, setCache
 
 
 class ChapterController(viewsets.ModelViewSet):
@@ -21,7 +22,7 @@ class ChapterController(viewsets.ModelViewSet):
         request.authentication = ["show"]
         super().initial(request, *args, **kwargs)
 
-    @method_decorator(cache_page(settings.CACHE_PAGE_IN_SECONDS))
+    # @method_decorator(cache_page(settings.CACHE_PAGE_IN_SECONDS))
     # @method_decorator(vary_on_headers("Authorization"))
     def show(self, request, slug, number):
         customer: Customer | None = request.user
@@ -39,8 +40,14 @@ class ChapterController(viewsets.ModelViewSet):
         chapter = chapterService.update(chapter.id, {"view_count": chapter.view_count + 1})
         chapterService.setViewCountCacheByChapter(chapter)
 
+        key = f"chapter_show_slug:{slug}_chapter_number:{number}"
+        response = getCache(key, None)
+        if response != None:
+            return response
+
         if chapter.type == CategoryEnum.NOVEL:
             chapter.text = chapterService.loadChapterTextFromStorage(chapter)
 
         response = ChapterResource(chapter, customer, status=status.HTTP_200_OK)
+        setCache(key, response, settings.CACHE_PAGE_IN_SECONDS)
         return response
