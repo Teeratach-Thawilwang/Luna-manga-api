548974426c5573e8cd284000f30ad473c006fa23
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-09-03 10:55:42 +0700

    Improve customer-profile, profile resource to reduce response time.

--- app/app/Http/Resources/Store/CustomerProfileController/CustomerProfileResource.py
+++ app/app/Http/Resources/Store/CustomerProfileController/CustomerProfileResource.py
@@ -1,17 +1,22 @@
+from django.http import JsonResponse
+
 from app.Domain.Customer.Models.Customer import Customer
 from app.Domain.File.Services.FileableService import FileableService
 from app.Enums.CollectionEnum import CollectionNameEnum
-from django.http import JsonResponse
 
 
 class CustomerProfileResource(JsonResponse):
     def __init__(self, customer: Customer, status=200, safe=False, json_dumps_params=None, **kwargs):
+        customer = Customer.objects.filter(id=customer.id)
+        customer = customer.prefetch_related("fileable__file")
+        customer = customer.first()
+
         self.data = {
             "id": customer.id,
             "email": customer.email,
             "nick_name": customer.nick_name,
             "first_name": customer.first_name,
             "last_name": customer.last_name,
-            "profile_image": FileableService().transformImagesByCollection(customer.fileable, CollectionNameEnum.PROFILE_IMAGE, "store"),
+            "profile_image": FileableService().transformImagesByCollection(customer.fileable.all(), CollectionNameEnum.PROFILE_IMAGE, "store"),
         }
         super().__init__(self.data, status=status, safe=safe, json_dumps_params=json_dumps_params, **kwargs)
