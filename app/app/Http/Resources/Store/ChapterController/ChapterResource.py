499d077474f9add3e1a69576c6241722b2e4bc3e
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-09-03 09:32:44 +0700

    Improve chapter controller to reduce response time.

--- app/app/Http/Resources/Store/ChapterController/ChapterResource.py
+++ app/app/Http/Resources/Store/ChapterController/ChapterResource.py
@@ -1,22 +1,25 @@
+from django.http import JsonResponse
+
 from app.Domain.Chapter.Models.Chapter import Chapter
 from app.Domain.Chapter.Services.ChapterService import ChapterService
 from app.Domain.Customer.Models.Customer import Customer
 from app.Domain.File.Services.FileableService import FileableService
 from app.Enums.CollectionEnum import CollectionNameEnum
-from django.http import JsonResponse
 
 
 class ChapterResource(JsonResponse):
     def __init__(self, chapter: Chapter, customer: Customer, status=200, safe=False, json_dumps_params=None, **kwargs):
         fileableService = FileableService()
         chapterService = ChapterService()
+        chapter = chapter.prefetch_related("fileable__file", "story__chapter")
+        fileables = chapter.fileable.all()
         self.data = {
             "id": chapter.id,
             "story_name": chapter.story.name,
             "type": chapter.type,
             "text": chapter.text,
-            "images": fileableService.transformImagesByCollection(chapter.fileable, CollectionNameEnum.STORY_IMAGE, "store"),
-            "audio": fileableService.transformAudioByCollection(chapter.fileable, CollectionNameEnum.CHAPTER_AUDIO, "store"),
+            "images": fileableService.transformImagesByCollection(fileables, CollectionNameEnum.STORY_IMAGE, "store"),
+            "audio": fileableService.transformAudioByCollection(fileables, CollectionNameEnum.CHAPTER_AUDIO, "store"),
             "reaction": chapterService.transformReactionByChapterAndCustomer(chapter, customer),
             "chapter_list": chapterService.getChapteListByStory(chapter.story),
         }
